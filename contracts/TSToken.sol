a28c0fc252778f2aa67b0b7990afb0abdf61b327
Christian Hentschel <chentschel@gmail.com>
2019-11-23 15:44:42 -0300

    fix: integrated referral managment and bulk send

--- contracts/TSToken.sol
+++ contracts/TSToken.sol
@@ -1,20 +1,55 @@
 pragma solidity ^0.5.12;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Pausable.sol";
 
+interface IPlasmaRoot {
+    function deposit(address tokenAddr, address user, uint256 amount) external;
+}
+
 contract TSToken is Ownable, ERC20Detailed, ERC20Pausable {
 
-    event DepositManagerChange(address depositManager);
+    using SafeERC20 for IERC20;
 
-    string public constant NAME = "TradeStars TS Utility Coin";
-    string public constant SYMBOL = "TS";
+    event PlasmaDeposit(address indexed from, uint256 amount);
+    event Redeemed(address indexed user, uint256 amount);
+
+    /// Token details
+    string public constant NAME = "TradeStars TSX Utility Coin";
+    string public constant SYMBOL = "TSX";
     uint8 public constant DECIMALS = 18;
 
-    /// We keep this allowed address for airdrops and deposits
-    address private depositManager;
+    /// Used to keep track of referrals
+    struct ReferralInfo {
+        uint256 qualifiedNonce;
+        uint256 accumulatedAmount;
+    }
+
+    mapping(address => ReferralInfo) private referralsInfo;
+
+    /// allowed address to send tokens from referral bonus
+    address private referralManager;
+
+    /// to easy keep tracking of burns
     uint256 public tokensBurned;
+
+    /**
+     * @dev Throws if called by any account other than the manger.
+     */
+    modifier onlyReferralManager() {
+        require(msg.sender == referralManager, "msg.sender is not referralManager");
+        _;
+    }
+
+    /**
+     * @dev Sets the referral contract address. Can only be called by admin
+     * @param _referralManager address
+     */
+    function setReferralManager(address _referralManager) external onlyOwner {
+        referralManager = _referralManager;
+    }
 
     /**
      * @dev Mints a specific amount of tokens.
@@ -47,35 +82,74 @@ contract TSToken is Ownable, ERC20Detailed, ERC20Pausable {
     }
 
     /**
-     * @dev Sets the referral contract address. Can only be called by admin
-     * @param _depositManager address
+     * @dev Returns referral info for caller
      */
-    function setDepositManagerAddress(address _depositManager) external onlyOwner {
-        depositManager = _depositManager;
-        emit DepositManagerChange(depositManager);
+    function getReferralInfo(address user) external view returns (uint256 nonce, uint256 amount) {
+        nonce = referralsInfo[user].qualifiedNonce;
+        amount = referralsInfo[user].accumulatedAmount;
     }
 
     /**
-     * @dev Atomically increases the allowance granted to `plasmaContract` by the owner.
-     *  can only be called by the referral contract setted by the admin.
-     * @param owner address of the tokens
-     * @param addedValue amount to temporary allow plasmaContract to transfer
-     * @param plasmaContract address that will take the tokens deposited
+     * @dev redeem tokens
+     * @param userAddr redeemer address
+     * @param qualifiedNonce of the user
+     * @param tokensPerCredit to redeem
+     * @param plasmaRoot to deposit
      */
-    function increaseAllowanceForDeposit(
-        address owner,
-        uint256 addedValue,
-        address plasmaContract
+    function redeemTokens(
+        address userAddr,
+        uint256 qualifiedNonce,
+        uint256 tokensPerCredit,
+        address plasmaRoot
+    )
+        external onlyReferralManager whenNotPaused
+    {
+        ReferralInfo storage referralInfo = referralsInfo[userAddr];
+
+        /// Check nonce
+        require(qualifiedNonce > referralInfo.qualifiedNonce, "qualifiedNonce is invalid");
+
+        /// Calculate net amount to send
+        uint256 amount = (qualifiedNonce - referralInfo.qualifiedNonce) * tokensPerCredit;
+        require(balanceOf(referralManager) >= amount, "referralAdmin has no balance");
+
+        referralInfo.qualifiedNonce = qualifiedNonce;
+        referralInfo.accumulatedAmount = referralInfo.accumulatedAmount.add(amount);
+
+        /// transfer TSX to user account
+        IERC20(this).safeTransfer(userAddr, amount);
+
+        /// Call plasma deposit()
+        _plasmaDeposit(userAddr, amount, plasmaRoot);
+
+        emit Redeemed(userAddr, amount);
+    }
+
+    /**
+     * @dev transfer tokens to multiple addresses
+     * @param _destArray Array of destination addresses
+     * @param _amountArray Array of amounts to transfer to each corresponding address.
+     */
+    function bulkTransfer(
+        address[] calldata _destArray,
+        uint256[] calldata _amountArray
     )
         external whenNotPaused
     {
-        uint256 allowance = allowance(owner, plasmaContract);
+        require(_destArray.length == _amountArray.length, "arrays should be of same lenght");
 
-        /// Check msg sender is our contract && allowance is 0
-        require(msg.sender == address(depositManager), "invalid from");
-        require(allowance == 0, "plasmaContract as allowance > 0");
+        for (uint x = 0; x < _amountArray.length; x++) {
+            IERC20(this).safeTransfer(_destArray[x], _amountArray[x]);
+        }
+    }
 
-        _approve(owner, plasmaContract, allowance.add(addedValue));
+    /**
+     * Atomically approve and transfer tokens to a plasma sidechain
+     * @param amount of tokens to transfer
+     * @param plasmaRoot address of the plasma contract
+     */
+    function plasmaDeposit(uint256 amount, address plasmaRoot) external whenNotPaused {
+        _plasmaDeposit(msg.sender, amount, plasmaRoot);
     }
 
     /**
@@ -86,5 +160,26 @@ contract TSToken is Ownable, ERC20Detailed, ERC20Pausable {
 
         ERC20Pausable.initialize(_owner);
         ERC20Detailed.initialize(NAME, SYMBOL, DECIMALS);
+    }
+
+    /**
+     * @dev Atomically increases the allowance and calls plasma deposit()
+     * @param owner user address
+     * @param amount amount to deposit
+     * @param plasmaRoot address of the plasma root contract
+     */
+    function _plasmaDeposit(address owner, uint256 amount, address plasmaRoot) private {
+
+        /// Aprove allowance
+        IERC20(this).safeApprove(plasmaRoot, amount);
+
+        /// Call plasma deposit
+        IPlasmaRoot(plasmaRoot).deposit(
+            address(this),
+            owner,
+            amount
+        );
+
+        emit PlasmaDeposit(owner, amount);
     }
 }
--- test/TSToken.test.js
+++ test/TSToken.test.js
@@ -1,7 +1,8 @@
 const { TestHelper } = require('@openzeppelin/cli');
 const { Contracts, ZWeb3, assertRevert } = require('@openzeppelin/upgrades');
 
-const { toWei, BN } = require('web3-utils');
+const { toWei } = require('web3-utils');
+const abi = require('ethereumjs-abi');
 
 ZWeb3.initialize(web3.currentProvider);
 
@@ -13,127 +14,212 @@ function checkEventName(tx, eventName) {
 }
 
 const TSToken = Contracts.getFromLocal('TSToken');
+const MockPlasma = Contracts.getFromLocal('MockContract');
 
-contract('VestingManager', ([_, owner, notOwner, party1, depositManager, plasmaAddr, anyone]) => {
+contract('TSToken', ([_, owner, notOwner, referralManager, someone, anotherOne]) => {
 
-    let token;
+  let token;
+  let plasmaRoot;
+
+  before(async function() {
+      const project = await TestHelper();
+
+      /// Create TS Token
+      token = await project.createProxy(TSToken, {
+        initMethod: 'initialize',
+        initArgs: [owner]
+      });
+
+      // create a mock
+      plasmaRoot = await MockPlasma.new({ gas: 4000000 });
+  });
+
+  describe('Mint/Burn tests', function() {
+
+    it(`should OK mint()`, async function() {
+      const amount = toWei('200');
+
+      const tx = await token.methods.mint(anotherOne, amount).send({
+        from: owner,
+        gas: 6721975,
+        gasPrice: 5e9
+      });
+
+      checkEventName(tx, "Transfer");
+    });
+
+    it(`should FAIL mint() :: not owner`, async function() {
+      const amount = toWei('200');
+
+      await assertRevert(
+        token.methods.mint(anotherOne, amount).send({
+          from: notOwner,
+          gas: 6721975,
+          gasPrice: 5e9
+        })
+      );
+    });
+
+    it(`should OK tokensBurned() counter`, async function() {
+      const amount = toWei('100');
+
+      const tx = await token.methods.burn(amount).send({
+        from: anotherOne,
+        gas: 6721975,
+        gasPrice: 5e9
+      });
+
+      checkEventName(tx, "Transfer");
+
+      const ret = await token.methods.tokensBurned().call();
+      ret.should.be.eq(String(amount));
+    });
+
+  });
+
+  describe('DepositManager tests', function() {
+
+    it(`should OK setReferralManager()`, async function() {
+      await token.methods.setReferralManager(referralManager).send({
+        from: owner,
+        gas: 6721975,
+        gasPrice: 5e9
+      });
+    });
+
+    it(`should FAIL setReferralManager() :: not owner`, async function() {
+      await assertRevert(
+        token.methods.setReferralManager(referralManager).send({
+          from: notOwner,
+          gas: 6721975,
+          gasPrice: 5e9
+        })
+      );
+    });
+  });
+
+  describe('Redeem tokens test', function() {
 
     before(async function() {
-        const project = await TestHelper();
-
-        /// Create TS Token
-        token = await project.createProxy(TSToken, {
-          initMethod: 'initialize',
-          initArgs: [owner]
-        });
+      /// mint tokens for referralManager
+      await token.methods.mint(referralManager, toWei('1000000')).send({
+        from: owner,
+        gas: 6721975,
+        gasPrice: 5e9
+      });
     });
 
-    describe('Mint/Burn tests', function() {
-
-      it(`should OK mint()`, async function() {
-        const amount = toWei('200');
-
-        const tx = await token.methods.mint(party1, amount).send({
-          from: owner,
-          gas: 6721975,
-          gasPrice: 5e9
-        });
-        checkEventName(tx, "Transfer");
+    /// Before each call to redem, set allowance to 0, because theres no
+    /// call from the plasmaRoot contract. (calls transferFrom on each deposit)
+    beforeEach(async function () {
+      await token.methods.approve(plasmaRoot.address, 0).send({
+        from: someone,
+        gas: 5000000
       });
-
-      it(`should FAIL mint() :: not owner`, async function() {
-        await assertRevert(
-          token.methods.mint(party1, owner).send({
-            from: notOwner,
-            gas: 6721975,
-            gasPrice: 5e9
-          })
-        );
-      });
-
-      it(`should OK tokensBurned() counter`, async function() {
-        const amount = toWei('100');
-        const tx = await token.methods.burn(amount).send({
-          from: party1,
-          gas: 6721975,
-          gasPrice: 5e9
-        });
-        checkEventName(tx, "Transfer");
-
-        const ret = await token.methods.tokensBurned().call();
-        ret.should.be.eq(String(amount));
-      });
-
     });
 
-    describe('DepositManager tests', function() {
+    it(`Should OK redeemTokens()`, async function() {
+      const qualifiedNonce = 1;
+      const tokensPerCredit = toWei('250');
 
-      it(`should OK setDepositManagerAddress()`, async function() {
-        const tx = await token.methods.setDepositManagerAddress(depositManager).send({
-          from: owner,
-          gas: 6721975,
-          gasPrice: 5e9
-        });
-
-        checkEventName(tx, "DepositManagerChange");
+      const tx = await token.methods.redeemTokens(
+        someone,
+        qualifiedNonce,
+        tokensPerCredit,
+        plasmaRoot.address
+      ).send({
+        from: referralManager,
+        gas: 6721975,
+        gasPrice: 5e9
       });
 
-      it(`should FAIL setDepositManagerAddress() :: not owner`, async function() {
-        await assertRevert(
-          token.methods.setDepositManagerAddress(depositManager).send({
-            from: notOwner,
-            gas: 6721975,
-            gasPrice: 5e9
-          })
-        );
+      checkEventName(tx, 'Redeemed');
+
+      const { nonce, amount } = await token.methods.getReferralInfo(someone).call();
+
+      nonce.should.be.eq(String(qualifiedNonce));
+      amount.should.be.eq(tokensPerCredit);
+    });
+
+    it(`Should OK redeem to non-consecutive nonce`, async function() {
+      const qualifiedNonce = 4;
+      const tokensPerCredit = toWei('300');
+
+      const totalTokensRedeemed = toWei(String( ((4 - 1) * 300) + 250 ));
+
+      const tx = await token.methods.redeemTokens(
+        someone,
+        qualifiedNonce,
+        tokensPerCredit,
+        plasmaRoot.address
+      ).send({
+        from: referralManager,
+        gas: 6721975,
+        gasPrice: 5e9
       });
 
-      it(`should OK increaseAllowanceForDeposit()`, async function() {
-        const amount = toWei('100');
+      checkEventName(tx, 'Redeemed');
 
-        await token.methods.increaseAllowanceForDeposit(
-          party1,
-          amount,
-          plasmaAddr
+      const { nonce, amount } = await token.methods.getReferralInfo(someone).call();
+
+      nonce.should.be.eq(String(qualifiedNonce));
+      amount.should.be.eq(totalTokensRedeemed);
+    });
+
+    it(`FAIL redeemTokens() :: nonce invalid (< last nonce)`, async function() {
+      const qualifiedNonce = 0;
+      const tokensPerCredit = toWei('250');
+
+      await assertRevert(
+        token.methods.redeemTokens(
+          someone,
+          qualifiedNonce,
+          tokensPerCredit,
+          plasmaRoot.address
         ).send({
-          from: depositManager,
+          from: referralManager,
           gas: 6721975,
           gasPrice: 5e9
-        });
-      });
-
-      it(`should FAIL increaseAllowanceForDeposit() :: invalid call address`, async function() {
-        const amount = toWei('100');
-
-        await assertRevert(
-          token.methods.increaseAllowanceForDeposit(
-            party1,
-            amount,
-            plasmaAddr
-          ).send({
-            from: anyone,
-            gas: 6721975,
-            gasPrice: 5e9
-          })
-        );
-      });
-
-      it(`should FAIL increaseAllowanceForDeposit() :: current allowance > 0`, async function() {
-        const amount = toWei('100');
-
-        await assertRevert(
-          token.methods.increaseAllowanceForDeposit(
-            party1,
-            amount,
-            plasmaAddr
-          ).send({
-            from: depositManager,
-            gas: 6721975,
-            gasPrice: 5e9
-          })
-        );
-      });
-
+        })
+      );
     });
+
+    it(`FAIL redeemTokens() :: nonce invalid (= last nonce)`, async function() {
+      const qualifiedNonce = 4;
+      const tokensPerCredit = toWei('250');
+
+      await assertRevert(
+        token.methods.redeemTokens(
+          someone,
+          qualifiedNonce,
+          tokensPerCredit,
+          plasmaRoot.address
+        ).send({
+          from: referralManager,
+          gas: 6721975,
+          gasPrice: 5e9
+        })
+      );
+    });
+
+    it(`FAIL redeemTokens() :: non referralManager call`, async function() {
+      const qualifiedNonce = 5;
+      const tokensPerCredit = toWei('250');
+
+      await assertRevert(
+        token.methods.redeemTokens(
+          someone,
+          qualifiedNonce,
+          tokensPerCredit,
+          plasmaRoot.address
+        ).send({
+          from: someone,
+          gas: 6721975,
+          gasPrice: 5e9
+        })
+      );
+    });
+
+  });
 
 });
