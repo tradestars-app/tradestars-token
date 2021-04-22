d8ca2a42c275b25bd8feb99bf3bf76bf7cb44b3a
Christian Hentschel <chentschel@gmail.com>
2021-04-22 02:17:01 -0300

    feat: hardhat, security review v1

--- contracts/tsx/TSX.sol
+++ contracts/tsx/TSX.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
 
 /**
  * @dev {TSX}:
@@ -20,7 +22,7 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
  * and pauser roles to other accounts.
  */
 
-contract TSX is AccessControlEnumerable, ERC20Snapshot, ERC20Burnable, ERC20Pausable {
+contract TSX is Context, AccessControlEnumerable, ERC20Snapshot, ERC20Burnable, ERC20Pausable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
@@ -29,10 +31,10 @@ contract TSX is AccessControlEnumerable, ERC20Snapshot, ERC20Burnable, ERC20Paus
     string public constant SYMBOL = "TSX";
 
     constructor() ERC20(NAME, SYMBOL) {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 
-        _setupRole(MINTER_ROLE, msg.sender);
-        _setupRole(PAUSER_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
     }
 
     /**
@@ -41,7 +43,7 @@ contract TSX is AccessControlEnumerable, ERC20Snapshot, ERC20Burnable, ERC20Paus
      * @param _value The amount of token to be minted.
      */
     function mint(address _to, uint256 _value) public virtual {
-        require(hasRole(MINTER_ROLE, msg.sender), "TSX: must have minter role to mint");
+        require(hasRole(MINTER_ROLE, _msgSender()), "TSX: must have minter role to mint");
         _mint(_to, _value);
     }
 
@@ -49,7 +51,7 @@ contract TSX is AccessControlEnumerable, ERC20Snapshot, ERC20Burnable, ERC20Paus
      * @dev Pauses all token transfers. The caller must have the `PAUSER_ROLE`.
      */
     function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, msg.sender), "TSX: must have pauser role to pause");
+        require(hasRole(PAUSER_ROLE, _msgSender()), "TSX: must have pauser role to pause");
         _pause();
     }
 
@@ -57,7 +59,7 @@ contract TSX is AccessControlEnumerable, ERC20Snapshot, ERC20Burnable, ERC20Paus
      * @dev Unpauses all token transfers. the caller must have the `PAUSER_ROLE`.
      */
     function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, msg.sender), "TSX: must have pauser role to unpause");
+        require(hasRole(PAUSER_ROLE, _msgSender()), "TSX: must have pauser role to unpause");
         _unpause();
     }
 
